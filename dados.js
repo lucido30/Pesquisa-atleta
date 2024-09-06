20dca57b7889ebc7d578e0f6b16c6e6f256da7f2
PEDRO HUMBERTO GAMA DE MEDEIROS <148107755+lucido30@users.noreply.github.com>
2024-09-06 17:59:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "rayssa leal",
+        descricao: "rayssa leal, a fadinha, é uma skatista Brasileira",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judoca judo judô brasileira judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca andrade é uma ginasta brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta brasileira ginastica ouro"
+    }
+]
